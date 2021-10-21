19ca5cae2d9a0e3204f18b9496b66918fd1f068d
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-10-21 23:01:30 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBuiOWmtr_EbsB3OfQjoAX-GxwgWS2trLM",
+    authDomain: "kwitternew-7f1d7.firebaseapp.com",
+    databaseURL: "https://kwitternew-7f1d7-default-rtdb.firebaseio.com",
+    projectId: "kwitternew-7f1d7",
+    storageBucket: "kwitternew-7f1d7.appspot.com",
+    messagingSenderId: "264974485724",
+    appId: "1:264974485724:web:6014484d0d038057670c19"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
