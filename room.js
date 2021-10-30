a0df973064a74f10a140d7d77fd3d34304dfb334
ibrar5 <83902973+ibrar5@users.noreply.github.com>
2021-10-30 22:34:19 +0600

    Add files via upload

--- room.js
+++ room.js
@@ -20,4 +20,14 @@ function addroom(){
     firebase.database().ref("/").child(room_name).upadate({
         purpose : "room"
     });
-}
+} 
+function getData() {
+  firebase.database().ref("/").on('value', function(snapshot) {document.getElementById("output").innerHTML = "";snapshot.forEach(function(childSnapshot) {childKey  = childSnapshot.key;
+       Room_names = childKey;
+      //Start code
+      console.log("room_name = " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick = 'redirect(this.id)'>#"+Room_names+"</div><hr>";
+      document.getElementById("output").innerHTML += row ;
+      //End code
+      });});}
+getData();
